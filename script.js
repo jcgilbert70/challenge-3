90857abf75eafb9aca6377a78d39a3bda4aada3a
Jeff <jcgilbert70@gmail.com>
2022-10-10 10:34:23 -0500

    added some pseudo code to get started

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+    console.log("button pressed")
+
+/*
+
+#1 prompt user for the password criteia
+    a) password length 8 < 128
+    b) include lowercase
+    c) include uppercase
+    d) include special characters
+#2 validate the input
+#3 generate password based on criteria
+#4 display password
+
+*/
+
+// (#4) code here, then return result var
+    return "generated password will go here";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +32,22 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dictionary = +'abcdefghijklmnopqrstuvqxyz';
+dictionary = +'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+dictionary = +'1234567890';
+dictionary = +'!@#$%^&*()_+-{}[];<>:';
