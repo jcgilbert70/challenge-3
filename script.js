95fdaaa6e045681571a6ab4f1a1544d40b46bbc8
Jeff <jcgilbert70@gmail.com>
2022-10-10 12:32:00 -0500

    deleted redundant variables in code

--- script.js
+++ script.js
@@ -20,13 +20,6 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// declarations
-var confirmLength = "";
-var confirmSpecialChar;
-var confirmNumericChar;
-var confirmLowercase;
-var confirmUppercase;
-
 // alert to inform user how to begin
 alert("Click on Generate Password to begin your random password creation")
 
